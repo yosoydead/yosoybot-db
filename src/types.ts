8ebba3d0bc7b7f645e8c47e9a84ff3e1e30278e1
Yosoydead <radu-bogdan@outlook.com>
2021-05-21 12:28:26 +0300

    ISSUE-2: added a route config type

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,19 @@
 import { RESPONSE_TYPE } from "./responseType";
+import { Request, Response, NextFunction} from "express";
 export interface ICustomJsonResponse {
   message: string,
   statusCode: number,
   status: RESPONSE_TYPE
+}
+
+export type API_OPERATIONS = "get" | "post" | "put" | "patch" | "delete";
+export interface ICustomRoute {
+  url: string;
+  routeHandler: (req: Request, res: Response, next: NextFunction, dbClient: IDbCommunication) => any;
+}
+
+export interface IRouteConfig {
+  (operation: API_OPERATIONS): [ICustomRoute]
 }
 
 export interface IDbCommunication {
