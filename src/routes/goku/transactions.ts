2a7da7fc18b1b7a44b078f92eb85838df555640c
Radu Bogdan-Mircea <radu-bogdan@outlook.com>
2021-09-16 15:47:33 +0300

    issue-2: added routes for production

--- src/routes/goku/transactions.ts
+++ src/routes/goku/transactions.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { addTransaction } from "../../controllers/genericTransactionController";
+import { addTransaction, getUsersBank, getUserTransactions } from "../../controllers/genericTransactionController";
 import { ICustomRoute } from "../../types";
 import { routeIterator } from "../../utils/routesIterator";
 
 const routesConfig: ICustomRoute[] = [
 	{ action: "post", url: "/goku/transactions/add", routeHandler: addTransaction },
+	{ action: "get", url: "/goku/transactions/getUsersBank", routeHandler: getUsersBank },
+	{ action: "get", url: "/goku/transactions/getUserTransactions/:id/:howMany", routeHandler: getUserTransactions }
 ];
 
 const transactionsRouter: Router =  routeIterator(routesConfig);
